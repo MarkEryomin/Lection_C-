45352c9e9c1801397b9763563ab8d904526112d1
MarkEryomin <erjomin.mark@gmail.com>
2023-01-12 11:46:20 +0300

    Finished of lection 3

---
+++ lection3/example012_2_ForText/Program.cs
@@ -0,0 +1,35 @@
+﻿// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёртачками,
+// маленькие буквы "к", заменить на большие "К", большие "С",на маленькие "с".
+
+string text= "-Я думаю,- сказал князь, улыбаясь,- что ,"
+            +"ежели бы вас послали вместо нашего милого Винценгероде,"
+            +"вы бы взяли приступом согласие прусского короля."
+            +"Вы так красноречивы. Вы дадите мне чаю?- С Сахаром?";
+
+// string s= "qwerty"
+//            012345
+// s[3] это r
+
+string Replace(string text,char oldValue,char newValue)
+{
+    string result = string.Empty;
+
+    int length= text.Length;
+    for ( int i =0; i<length; i++)
+    {
+        if (text[i] == oldValue) result=result + $"{newValue}";
+        else result=result + $"{text[i]}";
+        
+    }
+
+    return result;
+}
+string newText=Replace(text,' ','|');
+Console.WriteLine(newText);
+Console.WriteLine();
+string newText1=Replace(newText,'к','К');
+Console.WriteLine(newText1);
+Console.WriteLine();
+string newText2=Replace(newText1,'С','с');
+Console.WriteLine(newText2);
---
+++ lection3/example012_Method/Program.cs
@@ -0,0 +1,75 @@
+﻿  
+//   Вариант 1 ничего не принимают,ничего не отдают
+
+void Method1()
+{
+    Console.WriteLine("Автор...");
+}
+
+// Method1();
+
+
+
+//   Вариант 2 ничего не возвращают ,но могут принимать что-то
+
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2("Текст сообщения");
+
+void Method21(string msg, int count)
+{
+    int i=0;
+    while (i<count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+//Method21("Текст", 4 );
+// Method21(count: 4 , msg :"Новый текст");
+
+
+//   Вариант 3 что-то возвращает,но ничего не принимает
+int Method3()
+{
+    return DateTime.Now.Year;
+}
+
+int year =Method3();
+// Console.WriteLine(year);
+
+
+//   Вариант 4 что-то принимает и что-то отдаёт
+
+// string Method4 (int count, string text)
+// {
+//     int i =0;
+//     string result= string.Empty;
+
+//     while (i<count)
+//     {
+//         result=result+text ;
+//         i++;
+//     }
+//     return result ;
+
+// }
+
+string Method4 (int count, string text)
+{
+
+    string result= string.Empty;
+    for (int i =0;i<count;i++)
+    {
+        result=result+text ;
+    }
+    return result ;
+
+}
+string res = Method4(10, "xyz");
+Console.WriteLine(res);
+
+
+// цикл в цикле
