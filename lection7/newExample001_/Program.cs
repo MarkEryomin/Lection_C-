05db03f26f28116957b2ea61304374c43bc8a6cc
Mark <erjomin.mark@gmail.com>
2023-02-01 01:05:01 +0300

    end 7 seminar

---
+++ lection7/newExample001_/Program.cs
@@ -0,0 +1,40 @@
+﻿// Собрать строку с числами от a до b ,a<=b
+
+string NumbersFor(int a,int b)
+{
+    string result =String.Empty;
+    for(int i =a;i<=b;i++)
+    {
+        result+=$"{i} ";
+    }
+    return result;
+} 
+
+string NumbersRec(int a,int b)
+{
+    if (a<=b) return $"{a} "+NumbersRec(a+1,b);
+    else return String.Empty;
+}
+
+string NumbersRecRew(int a,int b)
+{
+    if (a<=b) return NumbersRecRew(a+1,b)+$"{a} ";
+    else return String.Empty;
+}
+
+string NumbersForRew(int a,int b)
+{
+    string result =String.Empty;
+    for(int i =b;i>=a;i--)
+    {
+        result+=$"{i} ";
+    }
+    return result;
+}
+
+Console.WriteLine(NumbersFor(1,10));
+Console.WriteLine(NumbersRec(1,10));
+
+
+Console.WriteLine(NumbersForRew(1,10));
+Console.WriteLine(NumbersRecRew(1,10));
---
+++ lection7/newExample002_Sum1-N/Program.cs
@@ -0,0 +1,17 @@
+﻿//  Сумма чисел от1 до N
+
+int SumFor(int n)
+{
+    int result=0;
+    for (int i=1;i<=n;i++) result+=i;
+    return result;  
+}
+
+int SumRec(int n)
+{
+    if(n==0) return 0;
+    else return n+SumRec(n-1);
+}
+
+Console.WriteLine(SumFor(12));
+Console.WriteLine(SumRec(13));
